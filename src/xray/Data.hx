5c6fce967203424acc1ede58789b041dad53e9f5
Simon Krajewski <simon@haxe.org>
2013-06-02 19:13:04 +0200

    serialize metadata

--- src/xray/Data.hx
+++ src/xray/Data.hx
@@ -1,5 +1,10 @@
 package xray;
 
+using haxe.macro.Tools;
+import haxe.macro.Context;
+import haxe.macro.Expr;
+using Lambda;
+
 class RefData<T>
 {
 	public static function of<T>(ref:T):RefData<T>
@@ -27,16 +32,29 @@ class RefData<T>
 
 class MetaData
 {
-	var impl:Bool;
-
-	public function new(impl:Bool)
+	var _meta:Array<{name: String, params:Array<String>, pos: Position}>;
+	
+	public function new(meta:Metadata)
 	{
-		this.impl = impl;
+		_meta = meta.map(function(m) return { name: new String(untyped m.name.__s), params: m.params.map(function(e) return e.toString()), pos: null});
 	}
 
 	public function has(name:String):Bool
 	{
-		return impl && name == ":impl";
+		return _meta.exists(function(m) return m.name == name);
+	}
+	
+	public function get():Metadata
+	{
+		return throw "niy";
+	}
+	
+	public function add(name:String, params:Array<Expr>, pos:Position) {
+		throw "niy";
+	}
+	
+	public function remove(name:String) {
+		throw "niy";
 	}
 }
 
