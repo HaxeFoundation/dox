f0746a3a99b154f339b2193859863ea64b2ddf40
Simon Krajewski <simon@haxe.org>
2014-02-25 19:04:00 +0100

    use localStorage instead of cookies

--- resources/index.js
+++ resources/index.js
@@ -1,21 +1,9 @@
 function createCookie(name, value, days) {
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-		var expires = "; expires=" + date.toGMTString();
-	} else var expires = "";
-	document.cookie = escape(name) + "=" + escape(value) + expires + "; path=/";
+	localStorage.setItem(name, value);
 }
 
 function readCookie(name) {
-	var nameEQ = escape(name) + "=";
-	var ca = document.cookie.split(';');
-	for (var i = 0; i < ca.length; i++) {
-		var c = ca[i];
-		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-		if (c.indexOf(nameEQ) == 0) return unescape(c.substring(nameEQ.length, c.length));
-	}
-	return null;
+	return localStorage.getItem(name);
 }
 
 function toggleInherited(el) {
