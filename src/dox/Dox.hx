8a397973672862e1c2f3fa4dcb2fcac101ae61c9
Simon Krajewski <simon@haxe.org>
2014-04-26 10:25:52 +0200

    add trailing slash to cwd so we do not choke in non-haxelib mode

--- src/dox/Dox.hx
+++ src/dox/Dox.hx
@@ -6,6 +6,7 @@ class Dox {
 	static public function main() {
 		// check if we're running from haxelib (last arg is original working dir)
 		var owd = Sys.getCwd();
+		owd = haxe.io.Path.addTrailingSlash(owd);
 		var args = Sys.args();
 		var last = new haxe.io.Path(args[args.length-1]).toString();
 		if (sys.FileSystem.exists(last) && sys.FileSystem.isDirectory(last)
@@ -19,31 +20,31 @@ class Dox {
 
 		cfg.outputPath = "pages";
 		cfg.xmlPath = "xml";
-		
+
 		var argHandler = hxargs.Args.generate([
 			["-r", "--document-root"] => function(path:String) throw 'The -r command is obsolete and can be omitted',
-			
+
 			@doc("Set the output path for generated pages")
 			["-o", "--output-path"] => function(path:String) cfg.outputPath = path,
-			
+
 			@doc("Set the xml input path")
 			["-i", "--input-path"] => function(path:String) cfg.xmlPath = path,
-			
+
 			@doc("Add template directory")
 			["-t", "--template-path"] => function(path:String) loadTemplates(cfg, path),
-			
+
 			@doc("Add a resource directory whose contents are copied to the output directory")
 			["-res", "--resource-path"] => function(dir:String) cfg.resourcePaths.push(dir),
-			
+
 			@doc("Add a path include filter")
 			["-in", "--include"] => function(regex:String) cfg.addFilter(regex, true),
-			
+
 			@doc("Add a path exclude filter")
 			["-ex", "--exclude"] => function(regex:String) cfg.addFilter(regex, false),
-			
+
 			@doc("Set the page main title")
 			["--title"] => function(name:String) cfg.pageTitle = name,
-			
+
 			@doc("Set the theme name or path")
 			["-theme"] => function(name:String) {
 				function setTheme(path:String) {
@@ -68,19 +69,19 @@ class Dox {
 				}
 				cfg.theme = setTheme(name);
 			},
-			
+
 			@doc("Defines key = value")
 			["-D", "--define"] => function(key:String, value:String) cfg.defines[key] = value,
-			
+
 			_ => function(arg:String) throw "Unknown command: " +arg
 		]);
-				
+
 		if (args.length == 0) {
 			Sys.println("Dox 1.0");
 			Sys.println(argHandler.getDoc());
 			Sys.exit(0);
 		}
-		
+
 		function sortArgs(args:Array<String>) {
 			var i = 0;
 			var args2 = [];
@@ -95,26 +96,26 @@ class Dox {
 					args2.push(args[i++]);
 				}
 			}
-			
+
 			if (!hasThemeArgument) {
 				args2.unshift("default");
 				args2.unshift("-theme");
 			}
 			return args2;
 		}
-		
+
 		argHandler.parse(sortArgs(args));
-		
+
 		var writer = new Writer(cfg);
-		
+
 		if (!sys.FileSystem.exists(cfg.xmlPath)) {
 			Sys.println('Could not read input path ${cfg.xmlPath}');
 			Sys.exit(1);
 		}
 		var parser = new haxe.rtti.XmlParser();
-		
+
 		var tStart = haxe.Timer.stamp();
-		
+
 		function parseFile(path) {
 			var name = new haxe.io.Path(path).file;
 			Sys.println('Parsing $path');
@@ -127,7 +128,7 @@ class Dox {
 			parser.process(xml, name);
 			cfg.platforms.push(name);
 		}
-		
+
 		if (sys.FileSystem.isDirectory(cfg.xmlPath)) {
 			for (file in sys.FileSystem.readDirectory(cfg.xmlPath)) {
 				if (!StringTools.endsWith(file, ".xml")) continue;
@@ -136,35 +137,35 @@ class Dox {
 		} else {
 			parseFile(cfg.xmlPath);
 		}
-		
+
 		Sys.println("Processing types");
 		var proc = new Processor(cfg);
 		var root = proc.process(parser.root);
-		
+
 		var api = new Api(cfg, proc.infos);
 		var gen = new Generator(api, writer);
-		
+
 		Sys.println("");
 		Sys.println("Generating navigation");
 		gen.generateNavigation(root);
-		
+
 		Sys.println('Generating to ${cfg.outputPath}');
 		gen.generate(root);
-		
+
 		Sys.println("");
 		Sys.println('Generated ${api.infos.numGeneratedTypes} types in ${api.infos.numGeneratedPackages} packages');
-		
+
 		for (dir in cfg.resourcePaths) {
 			Sys.println('Copying resources from $dir');
 			writer.copyFrom(dir);
 		}
-		
+
 		writer.finalize();
-		
+
 		var elapsed = Std.string(haxe.Timer.stamp() - tStart).substr(0, 5);
 		Sys.println('Done (${elapsed}s)');
 	}
-	
+
 	static function transformPackage(x:Xml, p1, p2) {
 		switch( x.nodeType ) {
 		case Xml.Element:
@@ -176,7 +177,7 @@ class Dox {
 		default:
 		}
 	}
-	
+
 	static function loadTemplates(cfg:Config, path:String) {
 		cfg.addTemplatePath(path);
 		if (!sys.FileSystem.exists(path)) {
