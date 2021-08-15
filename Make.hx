8c846a3f1cc1ccba672bd695fd35a58fbb521552
Jens Fischer <jensfischer95@gmail.com>
2021-08-15 20:56:44 +0200

    Take advantage of module-level functions in Make.hx

--- Make.hx
+++ Make.hx
@@ -1,27 +1,25 @@
-class Make {
-	static function main() {
-		var args = Sys.args();
-		if (args.length == 0) {
-			args = ["dox", "xml", "pages", "server"];
-		}
-		for (arg in args) {
-			switch arg {
-				case "dox":
-					Sys.command("npx haxe run.hxml");
+function main() {
+	var args = Sys.args();
+	if (args.length == 0) {
+		args = ["dox", "xml", "pages", "server"];
+	}
+	for (arg in args) {
+		switch arg {
+			case "dox":
+				Sys.command("npx haxe run.hxml");
 
-				case "xml":
-					Sys.command("npx haxe xml.hxml");
+			case "xml":
+				Sys.command("npx haxe xml.hxml");
 
-				case "pages":
-					Sys.command("npx lix run dox -o bin/pages -i bin/xml --include dox -D source-path https://github.com/HaxeFoundation/dox/tree/master/test");
+			case "pages":
+				Sys.command("npx lix run dox -o bin/pages -i bin/xml --include dox -D source-path https://github.com/HaxeFoundation/dox/tree/master/test");
 
-				case "server":
-					Sys.setCwd("bin/pages");
-					Sys.command("nekotools server");
+			case "server":
+				Sys.setCwd("bin/pages");
+				Sys.command("nekotools server");
 
-				case "package":
-					Sys.command("7z a -i!run.n -i!run.js -i!resources -i!themes -i!src -i!haxelib.json -i!README.md -i!LICENSE.md -i!CHANGELOG.md bin/haxelib.zip");
-			}
+			case "package":
+				Sys.command("7z a -i!run.n -i!run.js -i!resources -i!themes -i!src -i!haxelib.json -i!README.md -i!LICENSE.md -i!CHANGELOG.md bin/haxelib.zip");
 		}
 	}
 }
