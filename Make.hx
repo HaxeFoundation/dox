e37dba6b641a86897f2f8fabebbb998c47a91e61
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2024-09-26 15:51:41 -0400

    replace nekotools server with simple http-server

--- Make.hx
+++ Make.hx
@@ -15,8 +15,7 @@ function main() {
 				Sys.command("npx lix run dox -o bin/pages -i bin/xml --include dox -D source-path https://github.com/HaxeFoundation/dox/tree/master/test");
 
 			case "server":
-				Sys.setCwd("bin/pages");
-				Sys.command("nekotools server");
+				Sys.command("npx http-server bin/pages -c-1");
 
 			case "package":
 				Sys.command("7z a -i!run.n -i!run.js -i!resources -i!themes -i!src -i!haxelib.json -i!README.md -i!LICENSE.md -i!CHANGELOG.md bin/haxelib.zip");
